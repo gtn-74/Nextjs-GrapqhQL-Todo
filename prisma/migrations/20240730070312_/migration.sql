3182a1c0b4297dd1abfe74167a6e796b5b0ca157
gtn-74 <gtn.r74@gmail.com>
2024-07-30 16:04:57 +0900

    [query]一覧取得

--- prisma/migrations/20240730070312_/migration.sql
+++ prisma/migrations/20240730070312_/migration.sql
@@ -3,7 +3,7 @@ CREATE TABLE "Todo" (
     "id" TEXT NOT NULL,
     "content" TEXT NOT NULL,
     "done" BOOLEAN NOT NULL DEFAULT false,
-    "createdAt" TEXT NOT NULL,
+    "createAt" TEXT NOT NULL,
 
     CONSTRAINT "Todo_pkey" PRIMARY KEY ("id")
 );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,26 @@
 import "@/styles/globals.css";
+
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import type { AppProps } from "next/app";
 
+// アポロクライアント
+export const client = new ApolloClient({
+  // uri:エンドポイントを指定するGraphQLのプロパティ
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT,
+  // apollo clientがデータキャッシュを管理する方法を指定する。
+  // InMemoryCache；apollo clientに組み込まれたキャッシュの実装の一つ。
+  // クライアント側で取得したデータをメモリ内にキャッシュする
+  // 以後、メモリキャッシュから同じキャッシュを取得できるから同じデータのリクエストをする必要がなくなる。
+  cache: new InMemoryCache(),
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
+
+// Apollo Clientは、状態管理ライブラリ
+// apollo clientとは、データベースも含めて、データのCRUD操作ができる状態管理がライブラリ
